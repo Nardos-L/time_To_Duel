4cacd6dd8c3c2eae6d5b86ad775cda69e35728fd
Nardos Lengo <nardi.8199@gmail.com>
2021-07-06 20:33:53 -0700

    First commit

---
+++ TimeToDuel.js
@@ -0,0 +1,46 @@
+class Card {
+    constructor(name,cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+}
+
+class Unit extends Card {
+    constructor(name,cost,power,resilience) {
+        super(name,cost);
+        this.power = power;
+        this.resilience = resilience;
+    }
+    attack(target) {
+        target.resilience -= this.power;
+    }
+}
+
+class Effect extends Card {
+    constructor(name,cost,text,stat,magnitude){
+        super(name,cost);
+        this.text = text;
+        this.stat = stat;
+        this.magnitude = magnitude;
+    }
+    play(target) {
+        if( target instanceof Unit ) {
+            // implement card text here
+            console.log(target[this.stat]);
+            return target[this.stat] +=this.magnitude;
+        } else {
+            throw new Error( "Target must be a unit!" );
+        }
+    }
+}
+
+const redBeltNinja = new Unit('redBeltNinja',3,3,4);
+console.log(redBeltNinja);
+const blackBeltNinja = new Unit('blackBeltNinja',4,5,4);
+console.log(blackBeltNinja);
+redBeltNinja.attack(blackBeltNinja);
+console.log(blackBeltNinja);
+const hardAlgorithm = new Effect('hardAlgorithm',2,"increase target's resilience by 3",'resilience',3);
+console.log(hardAlgorithm)
+hardAlgorithm.play(redBeltNinja);
+console.log(redBeltNinja);
