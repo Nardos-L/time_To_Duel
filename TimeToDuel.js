7010e65b5625cfd8e20bec115cd0a4a4609e41df
Nardos Lengo <nardi.8199@gmail.com>
2021-07-06 20:54:41 -0700

    Added instance

--- TimeToDuel.js
+++ TimeToDuel.js
@@ -26,7 +26,7 @@ class Effect extends Card {
     play(target) {
         if( target instanceof Unit ) {
             // implement card text here
-            console.log(target[this.stat]);
+            //console.log(target[this.stat]);
             return target[this.stat] +=this.magnitude;
         } else {
             throw new Error( "Target must be a unit!" );
@@ -36,11 +36,27 @@ class Effect extends Card {
 
 const redBeltNinja = new Unit('redBeltNinja',3,3,4);
 console.log(redBeltNinja);
+
 const blackBeltNinja = new Unit('blackBeltNinja',4,5,4);
 console.log(blackBeltNinja);
+
 redBeltNinja.attack(blackBeltNinja);
 console.log(blackBeltNinja);
-const hardAlgorithm = new Effect('hardAlgorithm',2,"increase target's resilience by 3",'resilience',3);
+
+const hardAlgorithm = new Effect('hardAlgorithm',2,"increase target's resilience by 3",'resilience',+3);
 console.log(hardAlgorithm)
+
 hardAlgorithm.play(redBeltNinja);
 console.log(redBeltNinja);
+
+const UnhandledPromiseRejection = new Effect('UnhandledPromiseRejection',1,"reduce target's resilience by 2",'resilience',-2);
+console.log(UnhandledPromiseRejection);
+
+UnhandledPromiseRejection.play(redBeltNinja);
+console.log(redBeltNinja);
+
+const PairProgramming = new Effect('PairProgramming',3,"increase target's power by 2",'power',+2);
+console.log(PairProgramming);
+PairProgramming.play(redBeltNinja);
+console.log(redBeltNinja);
+
